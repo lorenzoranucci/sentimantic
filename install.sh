36aba9e11fe71ff2059d7dd406835d7c7683efaf
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-16 16:20:04 +0100

    Minor changes

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-git submodule update --init --recursive
-#cd snorkel/
-#pip install --requirement python-package-requirement.txt
-#chmod +x set_env.sh
-chmod +x install-parser.sh
+
 source activate py2Env
-source set_env.sh
-source install-parser.sh
-cd ..
+pip install --requirement python-package-requirement.txt
+git submodule update --init --recursive
+
 
 
 #Download Wikipedia dump
-mkdir ./data/wikipedia/dump/en
+mkdir ./data/wikipedia/dump/en -p
 wget -q -O ./data/wikipedia/dump/en/dump.xml.bz2 https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles1.xml-p10p30302.bz2
 
 
