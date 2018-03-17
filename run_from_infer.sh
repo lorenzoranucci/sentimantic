8a12e69fcea602cd019ef118cdaa74bed6444656
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-17 13:05:21 +0100

    Remove wrong path

--- run_from_infer.sh
+++ run_from_infer.sh
@@ -3,5 +3,5 @@
 source activate py2Env
 cd snorkel/sentimantic/
 #run pipeline
-python snorkel/sentimantic/complete_pipeline.py infer download extract label train test clear parallelism 32
+python complete_pipeline.py infer download extract label train test clear parallelism 32
 
