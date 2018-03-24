14f19acc4be1562b4ce49b9df0ab8ba4a2af367c
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-24 16:59:38 +0100

    introduce testing with brat

--- run_setup.sh
+++ run_setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-source activate py2Env
 cd snorkel/sentimantic/
 #run pipeline
-python gold_label_creator.py "http://dbpedia.org/ontology/birthPlace"
-./run_brat.sh
+python complete_pipeline.py setup parallelism 8
 
