6b91f13783ca4f85a6eff1e8baf19ed6ab929715
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-25 16:20:30 +0200

    Add page size parameter

--- run_from_labelling.sh
+++ run_from_labelling.sh
@@ -3,5 +3,5 @@
 
 cd snorkel/sentimantic/
 #run pipeline
-python complete_pipeline.py label train_gen train_disc test parallelism 32
+python complete_pipeline.py label train_gen train_disc test parallelism 64
 
--- run_from_parse.sh
+++ run_from_parse.sh
@@ -3,5 +3,5 @@
 
 cd snorkel/sentimantic/
 #run pipeline
-python complete_pipeline.py parse infer download extract label train_gen train_disc test  parallelism 64
+python complete_pipeline.py parse infer download extract label train_gen train_disc test  parallelism 64 page_size 1000000
 
