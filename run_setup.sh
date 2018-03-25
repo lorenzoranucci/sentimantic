7e8e5f78fc15b30338cc5c0750e6b03ec38c2866
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-25 13:26:47 +0200

    Add testing using gold labeled data

--- run_setup.sh
+++ run_setup.sh
@@ -4,3 +4,6 @@ cd snorkel/sentimantic/
 #run pipeline
 python complete_pipeline.py setup parallelism 8
 
+cd ./snorkel/snorkel/contrib/brat/brat-v1.3_Crunchy_Frog/data/
+sudo chown -R www-data:www-data ./*
+
