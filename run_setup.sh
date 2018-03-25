e206f6d2c8e6460acf922b561fca8b0de4398836
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-25 16:57:20 +0200

    Fix bug on chown

--- run_setup.sh
+++ run_setup.sh
@@ -2,8 +2,8 @@
 
 cd snorkel/sentimantic/
 #run pipeline
-python complete_pipeline.py setup parallelism 8
+python complete_pipeline.py setup parallelism 1
 
-cd ./snorkel/snorkel/contrib/brat/brat-v1.3_Crunchy_Frog/data/
-sudo chown -R www-data:www-data ./*
+cd ../snorkel/snorkel/contrib/brat/brat-v1.3_Crunchy_Frog/data/
+chown -R www-data:www-data ./*
 
