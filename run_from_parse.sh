fac0fe097b83be9c86afebc3339fb1906eb07fd4
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-26 22:24:47 +0200

    limit 50000 cands

--- run_from_parse.sh
+++ run_from_parse.sh
@@ -3,5 +3,7 @@
 
 cd snorkel/sentimantic/
 #run pipeline
-python complete_pipeline.py parse infer download extract setup label train_gen train_disc test  parallelism 64 page_size 100000
+python complete_pipeline.py parse infer download extract setup label train_gen train_disc test  parallelism 64 page_size 100000 limit 50000
 
+cd ../snorkel/contrib/brat/brat-v1.3_Crunchy_Frog/data/
+chown -R www-data:www-data ./*
