bfd5e81b950c930c0ec75d422eab062a3e53744f
Lorenzo Franco Ranucci <loryzizu@gmail.com>
2018-03-27 17:27:54 +0200

    Remove setup

--- run_from_extract.sh
+++ run_from_extract.sh
@@ -3,7 +3,4 @@
 
 cd snorkel/sentimantic/
 #run pipeline
-python complete_pipeline.py extract setup label train_gen train_disc test parallelism 64 page_size 50000 limit 50000
-
-cd ../snorkel/contrib/brat/brat-v1.3_Crunchy_Frog/data/
-chown -R www-data:www-data ./*
+python complete_pipeline.py extract label train_gen train_disc test parallelism 32 page_size 50000 limit 50000
